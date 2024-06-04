bd8a931273fd514fe5698450ff774f6d1fa237dc
Jahid Hasan <jahidsite0@gmail.com>
2024-06-04 09:27:25 +0600

    variables & types

---
+++ variables.py
@@ -0,0 +1,9 @@
+name = "Jahid Hasan"
+print(name)
+print(type(name))
+integerNumber = 10
+print(integerNumber)
+print(type(integerNumber))
+floatNumber = 19.36
+print(floatNumber)
+print(type(floatNumber))
